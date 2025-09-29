884a5b7a1f651df646581437e7157735a088e137
Mahesh-619Zeno <mahesh.ghadei@techolution.com>
2025-09-29 16:41:39 +0530

    improved and refectored the login functionality

--- public/js/login.js
+++ public/js/login.js
@@ -1,96 +1,89 @@
-document.addEventListener('DOMContentLoaded', function () {
-
-
+document.addEventListener('DOMContentLoaded', () => {
+  // Add any DOM-related initialization here if needed
 });
 
-function startfirebase() {
-    console.log("startup...");
+function startFirebase() {
+  console.log("Initializing Firebase...");
 
-    const firebaseConfig = {
-        apiKey: "AIzaSyC6Cq8c5jprpZYin5iB_KSAdatFbRPicfk",
-        authDomain: "ytbeam.firebaseapp.com",
-        databaseURL: "https://ytbeam-default-rtdb.firebaseio.com",
-        projectId: "ytbeam",
-        storageBucket: "ytbeam.appspot.com",
-        messagingSenderId: "161790539788",
-        appId: "1:161790539788:web:08effd61c5fb7f58a39629"
-      };
+  const firebaseConfig = {
+    apiKey: "AIzaSyC6Cq8c5jprpZYin5iB_KSAdatFbRPicfk",
+    authDomain: "ytbeam.firebaseapp.com",
+    databaseURL: "https://ytbeam-default-rtdb.firebaseio.com",
+    projectId: "ytbeam",
+    storageBucket: "ytbeam.appspot.com",
+    messagingSenderId: "161790539788",
+    appId: "1:161790539788:web:08effd61c5fb7f58a39629"
+  };
 
-    if (!firebase.apps.length) {
-        var app = firebase.initializeApp(firebaseConfig);
-        console.log("startup");
-    }
+  if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+    console.log("Firebase initialized.");
+  }
+}
+
+function getParams(url) {
+  const urlObj = new URL(url);
+  const params = {};
+  for (const [key, value] of urlObj.searchParams.entries()) {
+    params[key] = value;
+  }
+  return params;
 }
 
 function redirect() {
-    console.log("redirecting...");
-    let tags = getParams(window.location.href);
-    console.log(tags)
-    if (tags.v != undefined) {
-        newurl = "/room.html" + '?v=' + tags.v;
-        window.location.href = newurl;
-    }
-    else if(tags.page != undefined){
-        window.location.href = tags.page;
-    }
-    else {
-        newurl = "";
-        window.location.replace(window.location.protocol + '//' + window.location.hostname + ':' + location.port + '/joinroom.html');
-    }
+  console.log("Redirecting...");
+  const params = getParams(window.location.href);
+
+  if (params.v !== undefined) {
+    window.location.href = `/room.html?v=${encodeURIComponent(params.v)}`;
+  } else if (params.page !== undefined) {
+    window.location.href = params.page;
+  } else {
+    const base = `${window.location.protocol}//${window.location.hostname}`;
+    const port = window.location.port ? `:${window.location.port}` : '';
+    window.location.replace(`${base}${port}/joinroom.html`);
+  }
 }
 
 function login() {
+  startFirebase();
 
-    startfirebase();
+  firebase.auth().onAuthStateChanged(user => {
+    console.log("Auth state changed.");
+    if (user) {
+      redirect();
+    } else {
+      console.log("No user authenticated (expected on first login).");
+    }
+  });
 
-    firebase.auth().onAuthStateChanged((user) => {
-        console.log("auth changed");
-        if (user) {
-            redirect();
-        } else {
-            console.log("no user, this is expected");
-        }
-    });
-
-    gauth = new firebase.auth.GoogleAuthProvider();
-    // using the object we will authenticate the user.
-    firebase.auth().signInWithPopup(gauth);
+  const provider = new firebase.auth.GoogleAuthProvider();
+  firebase.auth().signInWithPopup(provider);
 }
 
-function anonlogin() {
-    startfirebase();
+function anonLogin() {
+  startFirebase();
 
-    firebase.auth().signInAnonymously()
-        .then(() => {
-            redirect();
-        })
-        .catch((error) => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("anon login error");
-        });
+  firebase.auth().signInAnonymously()
+    .then(() => {
+      redirect();
+    })
+    .catch(error => {
+      console.error("Anonymous login failed:", error.code, error.message);
+    });
 }
 
 function logout() {
-    startfirebase();
+  startFirebase();
 
-    firebase.auth().signOut();
-    console.log("logged out");
-    setTimeout(function(){
+  firebase.auth().signOut()
+    .then(() => {
+      console.log("Logged out successfully.");
+      setTimeout(() => {
         window.location.href = "https://ytbeam-landing.webflow.io";
-    }, 1000);
-    
+      }, 1000);
+    })
+    .catch(error => {
+      console.error("Logout failed:", error.code, error.message);
+    });
 }
-
-var getParams = function (url) {
-	var params = {};
-	var parser = document.createElement('a');
-	parser.href = url;
-	var query = parser.search.substring(1);
-	var vars = query.split('?').join(', ').split('&');
-	for (var i = 0; i < vars.length; i++) {
-		var pair = vars[i].split('=');
-		params[pair[0]] = decodeURIComponent(pair[1]);
-	}
-	return params;
-};
